3c1153378b45dbd2358f639adcf5808f0822b2b3
Benoit Asselin <contact@ab-d.fr>
2014-04-02 12:14:56 +0200

    Update ServiceLocatorFactory.php
    Disable constructor

--- src/ServiceLocatorFactory/ServiceLocatorFactory.php
+++ src/ServiceLocatorFactory/ServiceLocatorFactory.php
@@ -11,7 +11,12 @@ class ServiceLocatorFactory
      * @var ServiceManager
      */
     private static $serviceManager = null;
-
+    
+    /**
+     * Disable constructor
+     */
+    private function __construct() { }
+    
     /**
      * @throw ServiceLocatorFactory\NullServiceLocatorException
      * @return Zend\ServiceManager\ServiceManager
